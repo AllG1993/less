f70892f912fe3641bf6e677e2374896c09032025
AllG1993 <43466637+AllG1993@users.noreply.github.com>
2020-02-09 19:26:16 +0200

    работа с базами данных

--- DB.py
+++ DB.py
@@ -15,4 +15,50 @@ def create_table_users(name_table):
     conn_db.close()
 
 
-create_table_users('test_db.sqlite')
+def add_user(name_table):
+    conn_db = sqlite3.connect(name_table)
+    cursor = conn_db.cursor()
+    cursor.execute('''
+    INSERT INTO users
+    (name, email)
+    VALUES ("Petrov Petr", "petrov@gmail.com")''')
+    conn_db.commit()
+    conn_db.close()
+
+
+users = [
+    ('Анатолий Шишков', 'tolikebolik@gmail.com'),
+    ('Василий Иванов', 'vasyaivanov@gmail.com'),
+    ('Григорий Мединский', 'medy@gmail.com'),
+    ('Владимир Борисов', 'borisov@gmail.com'),
+    ('Екатерина Исакова', 'isakova@gmail.com'),
+    ('Анастасия Володина', 'nagibatormamok@gmail.com'),
+    ('Киркоров Филипп', 'fi@gmail.com'),
+    ('Надежда Кадышева', 'nadikad@gmail.com'),
+    ('Альберт Энштейн', 'vseotnositelno@gmail.com')
+]
+
+
+def add_many_user(name_table):
+    conn_db = sqlite3.connect(name_table)
+    cursor = conn_db.cursor()
+    cursor.executemany("INSERT INTO users (name, email) VALUES (?, ?)", users)
+    conn_db.commit()
+    conn_db.close()
+
+
+def get_data(name_table, email):
+    conn_db = sqlite3.connect(name_table)
+    cursor = conn_db.cursor()
+
+    cursor.execute("SELECT * FROM users")
+
+    data = cursor.fetchall()
+
+    for users in data:
+        print(users[0], users[1], users[2])
+
+    conn_db.close()
+
+
+get_data('test_db.sqlite', 'nadikad@gmail.com')
